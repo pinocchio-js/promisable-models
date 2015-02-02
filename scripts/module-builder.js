abf6043d67e05b029fa26dd0098a0199c00d247d
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2015-02-02 11:25:33 +0100

    Build module using a script

---
+++ scripts/module-builder.js
@@ -0,0 +1,29 @@
+var beautify   = require('js-beautify').js_beautify,
+    fs         = require('fs'),
+    handlebars = require('handlebars'),
+    nodefn     = require('when/node'),
+    when       = require('when'),
+    readFile   = nodefn.lift(fs.readFile);
+
+
+var TEMPLATE_PATH = 'scripts/templates/marionette_module_definition.js.hbs';
+var SRC_PATH      = 'src/index.js';
+
+var template = readFile(TEMPLATE_PATH);
+var src      = readFile(SRC_PATH);
+
+when.join(template, src)
+  .then(function(results){
+    var template = results[0].toString(),
+        code     = results[1].toString(),
+        templateFn, build;
+
+    templateFn = handlebars.compile(template);
+    
+    build = beautify(templateFn({
+      name: 'Marionette.Promisable',
+      module: code 
+    }));
+
+    process.stdout.write(build);
+  });

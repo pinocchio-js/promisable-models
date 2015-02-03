1e48064c169425057f6b08e67c7ff45a7f055e97
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2015-02-03 10:57:57 +0100

    Added to the interface for done

--- test/promisable_model_spec.js
+++ test/promisable_model_spec.js
@@ -1,6 +1,23 @@
 describe('Marionette.Promisable', function() {
   
-  it('should pass', function() {
+  var App = new Marionette.Application();
+  App.module('Entities', Pinocchio.Promisable);
 
+  var TestModel = Backbone.Model.extend({
+    paramRoot: 'test',
+    url: '/test',
+
+    initialize: function() {
+      App.Entities.Promisable.Mixin.call(this);
+    }
   });
+
+  describe('promise interface', function() {
+    var promisedModel = new TestModel();
+
+    it('responds to done', function() {
+      expect(promisedModel.done).to.be.ok();
+    });
+  });
+
 });

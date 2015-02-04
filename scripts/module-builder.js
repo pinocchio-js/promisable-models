cba2925c0344654dab803a514ab76acf31eada1e
Jean Carlos Meninno <jean-carlos.meninno@xing.com>
2015-02-04 11:22:59 +0100

    Use streams to build the final module

--- scripts/module-builder.js
+++ scripts/module-builder.js
@@ -1,41 +1,40 @@
-var beautify   = require('js-beautify').js_beautify,
-    fs         = require('fs'),
-    handlebars = require('handlebars'),
-    nodefn     = require('when/node'),
-    when       = require('when'),
-    readFile   = nodefn.lift(fs.readFile);
+var fs            = require('vinyl-fs'),
+    handlebars    = require('handlebars'),
+    through       = require('through2'),
+    streamqueue   = require('streamqueue'),
+    moduleBuilder = new streamqueue({objectMode: true});
 
 
-var TEMPLATE_PATH   = 'scripts/templates/marionette_module_definition.js.hbs',
-    MIXIN_PATH      = 'src/mixin.js',
-    MODEL_PATH      = 'src/model.js',
-    COLLECTION_PATH = 'src/collection.js';
+var registerPartials = through.obj(function(file, enc, cb) {
+  var partialName = file.path.replace(file.base, '');
 
-var template   = readFile(TEMPLATE_PATH),
-    mixin      = readFile(MIXIN_PATH),
-    model      = readFile(MODEL_PATH),
-    collection = readFile(COLLECTION_PATH);
+  handlebars.registerPartial(partialName, file.contents.toString());
+  cb();
+});
 
-when.join(template, mixin, model, collection)
-  .then(function(results){
-    var template   = results[0].toString(),
-        mixin      = results[1].toString(),
-        model      = results[2].toString(),
-        collection = results[3].toString(),
-        templateFn, build;
+var renderTemplate = function(locale) {
+  return through.obj(function(file, enc, cb) {
+    var template = handlebars.compile(file.contents.toString());
 
-    templateFn = handlebars.compile(template);
-    
-    build = beautify(templateFn({
-      globalNamespace : 'Pinocchio',
-      namespace       : 'Promisable',
-      mixin           : mixin,
-      model           : model,
-      collection      : collection
-    }), {
-      indent_size: 2,
-      indent_char: " "
-    });
-
-    process.stdout.write(build);
+    this.push(template(locale))
+    cb();
   });
+}
+
+
+var config = {
+  globalNamespace: 'Pinocchio',
+  namespace:       'Promisable'
+}
+
+var partials = fs.src('src/*.js')
+  .pipe(registerPartials)
+
+var templates = fs.src('scripts/**/*.js.hbs')
+
+moduleBuilder
+  .queue(partials)
+  .queue(templates)
+  .done()
+  .pipe(renderTemplate(config))
+  .pipe(process.stdout)
